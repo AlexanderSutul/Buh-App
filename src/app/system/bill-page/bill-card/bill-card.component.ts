11703cbb9da354bab92d5f95a668c7d8ea1c266f
Anastasiia Sergeeva <anastacsiiasergeeva@MacBook-Air-Anastacia.local>
2019-03-07 16:14:54 +1000

    Init commit.
    Commit just for init. Test description.

---
+++ src/app/system/bill-page/bill-card/bill-card.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Bill } from '../../shared/models/bill.model';
+
+@Component({
+  selector: 'app-bill-card',
+  templateUrl: './bill-card.component.html',
+  styleUrls: ['./bill-card.component.scss']
+})
+export class BillCardComponent implements OnInit {
+
+  @Input() bill: Bill;
+  @Input() currency: any;
+
+  dollar: number;
+  euro: number;
+
+  constructor() { }
+
+  ngOnInit() {
+    const rates = this.currency.Valute;
+    this.dollar = this.bill.value / rates.USD.Value;
+    this.euro = this.bill.value / rates.EUR.Value;
+  }
+
+}
---
+++ src/app/system/bill-page/currency-card/currency-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-currency-card',
+  templateUrl: './currency-card.component.html',
+  styleUrls: ['./currency-card.component.scss']
+})
+export class CurrencyCardComponent {
+
+  @Input() currency: any;
+  currencies: string[] = ['USD', 'EUR'];
+
+  dollar: number;
+  euro: number;
+}
